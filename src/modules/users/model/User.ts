8d11d246164351bd653f2b752ed33fe514ead4f4
Rogerio Pavan <rogerio_pavan@hotmail.com>
2021-05-04 22:17:31 -0300

    test: cria testes unitarios no UsersRepository

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,21 @@
 import { v4 as uuidV4 } from "uuid";
 
-class User {
-  // Complete aqui
+export type UserType = {
+  id: string
+  name: string
+  admin: boolean
+  email: string
+  created_at: Date
+  updated_at: Date
+}
+
+class User implements UserType{
+  id = uuidV4();
+  name: string;
+  admin = false;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
 }
 
 export { User };
--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,43 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user)
+    return user
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const found = this.users.find(user => user.id === id)
+    return found
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const found = this.users.find(user => user.email === email)
+    return found
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const index = this.users.findIndex(user => user.id === receivedUser.id)
+    const newUser = {
+      ...receivedUser,
+      admin: true,
+      updated_at: new Date()
+    }
+
+    this.users[index] =  newUser
+    return newUser
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users
   }
 }
 
