ec3fb920260d8a8eac118b1f757382a893501f76
Rogerio Pavan <rogerio_pavan@hotmail.com>
2021-05-04 23:27:27 -0300

    feat: creates createUser useCase

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,16 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findByEmail(email)
+    
+    if(user){
+      throw Error('E-mail already registered')
+    }
+
+    return this.usersRepository.create({
+      email,
+      name,
+    })
   }
 }
 
